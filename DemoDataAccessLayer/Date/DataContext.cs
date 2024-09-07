81f93894e29d280b26836659b0c6f079b5de1e46
blyatsho <ma5740@fayoum.edu.eg>
2024-09-07 13:55:19 +0100

    Video 02

---
+++ DemoDataAccessLayer/Date/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using DemoDataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DemoDataAccessLayer.Date
+{
+    internal class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("connection string");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ DemoDataAccessLayer/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DemoDataAccessLayer.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; } // Pk
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public DateTime MyProperty { get; set; }
+    }
+}
