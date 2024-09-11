07e0dad84e13bf1eb2f72376cfdc0f0fe06a3fed
blyatsho <ma5740@fayoum.edu.eg>
2024-09-11 20:28:10 +0100

    Client Side Validation

--- DemoDataAccessLayer/Models/Employee.cs
+++ DemoDataAccessLayer/Models/Employee.cs
@@ -5,7 +5,9 @@ namespace DemoDataAccessLayer.Models
     public class Employee
     {
         public int Id { get; set; }
+        [StringLength(maximumLength: 50 , MinimumLength = 5)]
         public string Name { get; set; }
+        [Range(19,60)]
         public int Age { get; set; }
         public string Address { get; set; }
         [DataType(DataType.Currency)]
